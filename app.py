749a802d5d16626ab1da47effbb870fb8d1c3ee8
Yuchen Liu <steny138@gmail.com>
2016-10-09 02:44:27 +0800

    finish test send message

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def botHook_tpebus():
         print update
         message = update.message
         text = u'您剛剛輸入的指令是：' + message.text
-        bot.sendMessage(chat_id=message.chat.id, text=str(update))
+        bot.sendMessage(chat_id=message.chat.id, text=text)
         return 'ok'
     except Exception as e:
         print e
