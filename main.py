7571f09570ae167eb2d4221aa4cf7b73b6796854
Yuchen Liu <steny138@gmail.com>
2016-10-08 18:36:35 +0800

    fixed the issue that not show with compress data  from taipei open data

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
+import StringIO
 import urllib3
 import gzip
 import json
-import StringIO
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!2"
-
-@app.route("/youbike")
-def youbike():
-    # configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-    url = "http://data.taipei/youbike"
-    data = send_request(url)
-    bikeDict = json.loads(data)        
-    return data #  bikeDict['retVal']
-    
-@app.route("/bus/route")
 def busroute():
     try:
+        urllib3.disable_warnings()
         url = "http://data.taipei/bus/ROUTE"
+        
         data = send_request(url, compress=True)
-        routeDict = json.loads(data)        
-        return data #  bikeDict['retVal']
+        print data #  bikeDict['retVal']
     except Exception as e:
+        print 123
         print e
 
 def send_request(url, compress = False):
@@ -46,5 +32,6 @@ def decompressGzip(compressStr):
     decompressedStream = gzip.GzipFile(fileobj=compressedStram, mode='rb')
     result = decompressedStream.read()
     return result
+
 if __name__ == "__main__":
-    app.run()
+    busroute();
