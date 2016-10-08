1102ce884f46861ae67e652e4bd6a78eb0700931
Yuchen Liu <steny138@gmail.com>
2016-10-08 16:18:54 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import urllib3
+import gzip
+import json
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/youbike")
+def youbike():
+	# configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+    urllib3.disable_warnings()
+    url = "http://data.taipei/youbike"
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    
+    data = r.data
+    
+    r.release_conn()
+    bikeDict = json.loads(data)
+    return data# bikeDict['retVal']
+
+if __name__ == "__main__":
+    app.run()
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = open('./README.md', 'r').read()
+description = ''
+
+setup(name='ptt_crawler',
+      version='1.0.0',
+      description=description,
+      long_description=long_description,
+      author='YUCHEN LIU',
+      author_email='steny138@gmail.com',
+      url='https://github.com/steny138/PTTCrawler',
+      packages=['ptt_crawler'],
+      package_data={'ptt_crawler': ['*.csv']},
+      include_package_data=True,
+      license='LICENSE',
+      keywords=" " + \
+               "",
+      install_requires=['python-dateutil==1.5', 'ujson', 'urllib3'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: Chinese (Traditional)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Office/Business :: Financial :: Investment',
+          ],
+     )
