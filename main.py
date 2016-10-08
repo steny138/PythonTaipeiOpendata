7dc2bba75f05fb0ecabe3ceb12ebd5421b9c835f
Yuchen Liu <steny138@gmail.com>
2016-10-08 17:00:53 +0800

    deploy to heroku with flask

--- main.py
+++ main.py
@@ -4,17 +4,7 @@ import gzip
 import json
 
 def main():
-    # configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-    urllib3.disable_warnings()
-    url = "http://data.taipei/youbike"
-    http = urllib3.PoolManager()
-    r = http.request('GET', url)
     
-    data = r.data
-    
-    r.release_conn()
-    bikeDict = json.loads(data)
-    print data# bikeDict['retVal']
     
 if __name__ == "__main__":
     main()
